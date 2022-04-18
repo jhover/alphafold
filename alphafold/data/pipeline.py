73ecb9633d507cb125292bc3e6dfb892d971ab6f
jhover <jhover@localhost>
2022-04-17 21:35:23 -0400

    Update.

--- alphafold/data/pipeline.py
+++ alphafold/data/pipeline.py
@@ -186,6 +186,7 @@ class DataPipeline:
       try:
         with open( f'{self.cache_dir}/{seq_id}/sequence_features.pkl' , 'rb') as cf:
           sequence_features = pickle.load(cf)
+          logging.debug(f'loaded sequence_features.pkl')
       except Exception:
         logging.error(f'unable to load sequence_features.pkl via pickle from {self.cache_dir}/{seq_id}/')
         traceback.print_exc(file=sys.stdout)  
@@ -193,13 +194,15 @@ class DataPipeline:
       try:
         with open( f'{self.cache_dir}/{seq_id}/msa_features.pkl' , 'rb') as cf:
           msa_features = pickle.load(cf)
+          logging.debug(f'loaded msa_features.pkl')
       except Exception:
         logging.error(f'unable to load msa_features.pkl via pickle from {self.cache_dir}/{seq_id}/')
         traceback.print_exc(file=sys.stdout)
         
       try:  
         with open( f'{self.cache_dir}/{seq_id}/templates_result.pkl' , 'rb') as cf:
-          templates_result = pickle.load()        
+          templates_result = pickle.load()
+          logging.debug(f'loaded templates_result.pkl')        
       except Exception:
         logging.error(f'unable to load templates_result.pkl via pickle from {self.cache_dir}/{seq_id}/')
         traceback.print_exc(file=sys.stdout)    
